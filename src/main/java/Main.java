00372789778d77a9b52e71d514498c8b5a1a4d32
Skatu <44134991+Skatu@users.noreply.github.com>
2024-09-15 16:19:43 +0100

    support classes as object variables

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,24 +2,44 @@ import org.apache.velocity.VelocityContext;
 import org.apache.velocity.app.Velocity;
 import org.apache.velocity.app.VelocityEngine;
 
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.Writer;
-import java.lang.reflect.Field;
-import java.util.ArrayList;
+import java.io.*;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
 
-    private static final String template ="src/main/resources/velocity/template.vm";
+    private static final String TEMPLATE ="src/main/resources/velocity/template.vm";
+    private static final String FILE_NAME ="output.graphqls";
 
     public static void main(String[] args) throws IOException {
-        d223(Book.class);
+        var loader = ClassLoader.getSystemClassLoader();
+        var streamReader = loader.getResourceAsStream("model");
+        assert streamReader != null;
+        BufferedReader reader = new BufferedReader(new InputStreamReader(streamReader));
+        List<Class> classes = reader.lines().filter(line -> line.endsWith(".class")).map(line-> getClass("model",line)).collect(Collectors.toList());
+        generateFile(classes);
     }
 
-    private static void d223(Class<?> clazz) throws IOException {
+    private static void generateFile(List<Class> classes) throws IOException {
+        VelocityEngine ve = new VelocityEngine();
+        ve.init();
+        Writer writer = new FileWriter(FILE_NAME);
+        VelocityContext context = new VelocityContext();
+        context.put("classes", classes.stream().map(Class::getSimpleName).collect(Collectors.toList()));
+        for(var clazz: classes){
+            var fields = getFields(clazz);
+            context.put("className", clazz.getSimpleName());
+            context.put("fields", fields);
+            Velocity.mergeTemplate(TEMPLATE,"UTF-8", context, writer);
+            writer.flush();
+        }
+
+        writer.close();
+    }
+
+    private static void writeToFile(Class<?> clazz) throws IOException {
         var fields = getFields(clazz);
-        generateFile(clazz.getSimpleName(), fields, "output.graphqls");
+        generateFile(clazz.getSimpleName(), fields, FILE_NAME);
     }
 
     private static void generateFile(String className, Entry[] fields, String outputFileName) throws IOException {
@@ -31,7 +51,7 @@ public class Main {
         context.put("fields", fields);
 
         Writer writer = new FileWriter(outputFileName);
-        Velocity.mergeTemplate(template,"UTF-8", context, writer);
+        Velocity.mergeTemplate(TEMPLATE,"UTF-8", context, writer);
         writer.flush();
         writer.close();
     }
@@ -43,5 +63,13 @@ public class Main {
             entries[i] = Entry.create(fields[i]);
         }
         return entries;
+    }
+
+    private static Class<?> getClass(String packageName, String className){
+        try {
+            return Class.forName(packageName+"."+className.substring(0, className.lastIndexOf('.')));
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- src/main/java/model/Book.java
+++ src/main/java/model/Book.java
@@ -1,7 +1,9 @@
+package model;
+
 public class Book {
     private String id;
     private String title;
-    private String author;
+    private Author author;
     private int year;
     private double price;
     private boolean inSale;
